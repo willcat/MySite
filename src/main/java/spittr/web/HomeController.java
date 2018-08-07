e77f084e0129df7d81607a88b96c4643e95cf1ed
willcat <miaoshoubazi@yeah.net>
2018-08-07 19:47:31 +0800

    定义类级别的请求处理

--- src/main/java/spittr/web/HomeController.java
+++ src/main/java/spittr/web/HomeController.java
@@ -6,9 +6,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import static org.springframework.web.bind.annotation.RequestMethod.*;
 
 @Controller
+@RequestMapping({"/","/homepage"})//将控制器映射到"/"和"homepage"
 public class HomeController {
 	
-	@RequestMapping(value="/", method=GET)//默认就是GET
+	@RequestMapping(method=GET)//对控制器mapping做请求方法的补充
 	public String home() {
 		return "home"; //返回视图名字
 	}
